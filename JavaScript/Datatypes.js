0e1559e70b167690fa7316eb8e2782bf5541f954
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-14 10:28:15 +0530

    Add files via upload

---
+++ JavaScript/Datatypes.js
@@ -0,0 +1,43 @@
+//Data Types
+
+//string
+// let fName = "Kreetan"
+// // console.log(fName)
+// console.log(typeof (fName))
+// let num = "1234"
+// console.log(num)
+// console.log(typeof (num))
+
+//number
+// let age = 24;
+// let price = 20.54
+// console.log(typeof age)
+// console.log(typeof price)
+
+//boolean
+// let isLoggedIn = true;
+// let hasAccess = false;
+// console.log(typeof isLoggedIn)
+// console.log(typeof hasAccess)
+
+// //undefined
+// let marks;
+// console.log(marks)
+
+//null  // Represents intentional absence of value
+// let user = null;
+// console.log(user)
+// console.log(typeof user) //homework
+
+//bigint
+// let bigNum = 1234567812345672345672345623456n;
+// console.log(bigNum)
+// console.log(typeof bigNum)
+// let x = BigInt("234564324564321345643213456432")
+// console.log(x)
+
+//symbol
+// let id = Symbol("id")
+// let ic = Symbol("id")
+// console.log(id)
+// console.log(id==ic)
