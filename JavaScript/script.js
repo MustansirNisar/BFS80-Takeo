ad3ea394c30f968ca3ef65454253ba2c4588ff63
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-14 10:28:32 +0530

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,44 @@
+// var num; //Declaration
+// num = 10; //Initialization
+
+// var num = 20; //Declaration + Initialization
+
+// let num;
+// num = 30;
+
+// const num = 40;
+
+//var allows Redeclaration and updation.
+// var num = 10;
+// // console.log(num)
+// var num; //Redeclaration
+// num = 20; //Updation or Reinitialization
+// console.log(num)
+
+//let does not allow Redeclaration but allows updation
+// let num = 20;
+// // console.log(num)
+// // let num;
+// num = 30;
+// console.log(num)
+
+// const does not allow Redeclaration and updation
+// const num = 40;
+// // const num; // Not Allowed
+// num = 30; // Not Allowed
+// console.log(num)
+
+// let name; //valid
+// let age3;//valid
+// let _temp;//valid
+// let $price;//valid
+
+// let 1name; //Invalid
+
+// let var = 10; //Invalid as it is a reserved keyword
+
+//Case Sensitive
+// let user = "Hemanth"
+// let User = "Kabita"
+// console.log(user)
+// console.log(User)
