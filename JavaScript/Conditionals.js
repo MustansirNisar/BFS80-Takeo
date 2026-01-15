abaf9d8f90216638cd9403c8743f8ce1dfd4e2f4
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-15 09:44:36 +0530

    Add files via upload

---
+++ JavaScript/Conditionals.js
@@ -0,0 +1,57 @@
+//if statement
+// if(condition){
+//     code to execute if condition is true
+// }
+// else{
+//     code if the condition is false
+// }
+
+// let age = 20;
+// if(age < 18){
+//     console.log("You are an adult")
+//     console.log("Welcome to our session")
+// }
+// else{
+//     console.log("Your age is greater than 18")
+// }
+
+// console.log("How are you doing?")
+
+
+// if...else if...else ladder
+// if(condition1){
+//     code if condition1 is true
+// }
+// else if(condition2){
+//     code if condition2 is true
+// }
+// else{
+//     code if none of the conditions are true
+// }
+
+
+// let score = 90;
+// if(score>=90){
+//     console.log("Grade: A")
+// }
+// else if(score>=80){
+//     console.log("Grade: B")
+// }
+// else if(score>=70){
+//     console.log("Grade: C")
+// }
+// else{
+//     console.log("You did not qualify the exam")
+// }
+
+
+
+
+//Ternary operator
+// condition? value_if_true:value_if_flase
+let age = 10;
+// age>18?console.log("You are an adult"):console.log("You are a child")
+age>18?console.log("You are an adult"):
+age>15?console.log("greater than 15 but less than 18"):
+age>12?console.log("grtr than 12"):console.log("You are 10")
+
