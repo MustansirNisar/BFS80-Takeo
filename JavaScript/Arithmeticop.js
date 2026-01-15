123e6d072df82a0be0db2fdd1cfe71632976cb58
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-15 09:43:49 +0530

    Add files via upload

---
+++ JavaScript/Arithmeticop.js
@@ -0,0 +1,39 @@
+// let a = 10;
+// let b = 3;
+// let sum = a + b
+// console.log(sum)
+// console.log(a + b) //Addition
+// console.log(a - b) //Subtraction
+// console.log(a * b) //Multiplication
+// console.log(a / b) //Division: Quotient of division
+// console.log(a % b) //Modulus: Remainder of division
+// console.log(a ** b) //Exponentiation
+
+// let x = 5;
+// x++; //Adds 1 to the number(x)....Increment
+// console.log(x)
+// x--; //Subtracts 1 from a number(x)....Decrement
+// console.log(x)
+
+// let x = 5;
+// console.log(x++) //prints 5, then x becomes 6;
+// console.log(x)
+// console.log(++x);
+// console.log(x--);
+// console.log(x)
+// console.log(x++)
+// console.log(++x)
+// console.log(x)
+// console.log(x--)
+// console.log(--x)
+// console.log(x)
+
+
+//Unary Plus Operator.....Converts its operand to a number(if it isn't already)
+// let a = "1234"
+// // console.log(typeof a)
+// let b = +a;
+// console.log(a + 2) //string
+// console.log(b + 2) //number
+// console.log(+true)
+// console.log(+false)
