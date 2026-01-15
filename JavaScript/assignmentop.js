58ebf6fbff43a2ce7828bd098e186813454291e8
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-15 09:44:07 +0530

    Add files via upload

---
+++ JavaScript/assignmentop.js
@@ -0,0 +1,15 @@
+// let a = 10;
+// // a = a + 5;
+// // a = a - 5
+// // a = a / 2
+// a = a * 2
+// console.log(a)
+
+// let a = 10;
+// a += 5;
+// a -= 5
+// a /= 5
+// a *= 2
+// a %= 3
+// a **= 2  //Compound Assignment Operators
+// console.log(a)
