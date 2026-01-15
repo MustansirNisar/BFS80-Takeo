af306f0d20d070422425acc711e5528f71ceeed1
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-15 09:44:52 +0530

    Add files via upload

---
+++ JavaScript/logicalop.js
@@ -0,0 +1,12 @@
+let age = 30;
+let marks = 80;
+// console.log(age>18 && marks>75) //true
+// console.log(age>18 && marks>90) //false
+// console.log(age>50 && marks>90) //false
+// console.log(age>18 || marks>70) //true
+// console.log(age>18 || marks>90) // true
+// console.log(age>40 || marks>90) // false
+
+// console.log(!(age<marks)) //false
+
+
