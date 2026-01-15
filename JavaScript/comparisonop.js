f5fc284427b2807dee1754d0dbbc21a04a4b1ed2
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-15 09:44:21 +0530

    Add files via upload

---
+++ JavaScript/comparisonop.js
@@ -0,0 +1,31 @@
+// let a = 10;
+// let b = 5;
+// let c = "10";
+// console.log(a == c)  //true...checks only the value
+// console.log(a === c) //false...checks both the value and type
+// console.log(a != c)
+// console.log(a !== c)
+// console.log(a == c)
+
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a >= b)
+// console.log(a <= b)
+// console.log(a == b)
+// console.log(a != b)
+
+//Type Coercion
+// let a = 10;
+// let b = 5;
+// let c = "5";
+// let d = "abc"
+
+// console.log(a+b)
+// console.log(a+c)
+// console.log(a-c)
+// console.log(a+d)
+// // console.log(c-d)
+
+// let a = "1234"
+// let b = Number(a)
+// console.log(typeof b)
