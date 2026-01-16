5aed6b5518b769d344fa57eaae270a5e335a7a97
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-16 09:48:09 +0530

    Add files via upload

---
+++ JavaScript/functions.js
@@ -0,0 +1,105 @@
+//function definition or function declaration
+// function functionName(){
+//     code to run
+// }
+
+
+// function greet(){
+//     console.log("Hello and Welcome")
+//     console.log("How are you doing?")
+// }
+
+// //Function Call or Invocation
+// greet();
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// console.log("Learning Functions")
+// greet();
+
+// function greet(){
+//     for(let i=0;i<=10;i++){
+//     console.log("Hello and Welcome")
+// }
+// }
+
+// greet();
+// console.log("lines of code")
+// console.log("lines of code")
+// console.log("lines of code")
+// console.log("lines of code")
+// greet();
+
+
+//Function with Parameters
+// function greet(firstName){
+//     console.log("Welcome ", firstName)
+// }
+// greet("kabita");
+// greet("Sarbesh");
+// greet("Hemanth");
+
+// function add(a,b){
+//     console.log("The sum of the numbers is: ", a+b)
+// }
+// add(4,3);
+// add(10,20);
+// add(10,100);
+
+// return statement
+// function add(a,b){
+//     return a+b;       //return value
+// }
+
+// // let sum =  add(4,5);
+// // console.log(sum)
+
+// console.log(add(5,4));
+
+// function num(n){
+//     if(n>0) return "Positive";
+//     if(n<0) return "Negative";
+//     return "Zero"
+// }
+// console.log(num(-2))
+
+// function num(n){
+//     console.log("Before return statment")
+//     return n*2
+//     console.log("After the return statement")
+// }
+// console.log(num(8))
+// console.log(n)
+
+//Anonymous functions
+// let greet = function (){
+//     console.log("How are you?")
+// }
+
+// greet();
+
+// let greet = function (fName){
+//     console.log("How are you?", fName)
+// }
+
+// greet("Kreetan");
+
+//Arrow function
+// let greet = ()=>{
+//     console.log("Hello")
+// }
+// greet();
+
+// let cube = a => a**3
+// console.log(cube(2))
