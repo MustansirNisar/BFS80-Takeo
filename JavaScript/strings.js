496d2fa3c34a06ff77edfd46ffb0eb1fa6b7a4b9
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-16 09:49:03 +0530

    Add files via upload

---
+++ JavaScript/strings.js
@@ -0,0 +1,43 @@
+// let fName = "Sarbesh";
+// console.log(fName.length)  //Returns the number of characters in a string including white spaces
+// console.log(fName[2])
+// console.log(fName[3])
+
+//tuUpperCase() and toLowerCase....convert the string to upper or lower case
+// let city = "Srinagar"
+// city.toUpperCase()
+// console.log(city)
+// let newCity = city.toUpperCase();
+// console.log(newCity)
+// console.log(city.toUpperCase());
+// console.log(city.toLowerCase());
+
+// trim()...removes the whitespaces from both ends
+// let text = "  Something   "
+// console.log(text)
+// console.log(text.trim())
+
+//slice(start, end)....extracts a protion of the string
+// let text = "Something"
+// console.log(text.slice(0,4))
+// console.log(text.slice(-4,-2))
+// console.log(text.slice(3))
+
+// concat()....joins two or more strings
+// let str1 = "JavaScript"
+// let str2 = "React"
+// console.log(str1.concat(" ",str2))
+
+// replace()....returns a new string with the first occrence of a substring replaced by a new value
+// let text = "I like cars as cars are very comfortable"
+// console.log(text.replace("cars", "bikes"))
+// console.log(text.replaceAll("cars", "bikes"))
+
+//indexof()...returns the position of the first occurence of a substring
+// let text = "JavaScript"
+// console.log(text.indexOf("a"))
+// console.log(text.indexOf("aSc"))
+
+// charAt().....returns a char at a specific index
+let text = "JavaScript"
+console.log(text.charAt(3))
