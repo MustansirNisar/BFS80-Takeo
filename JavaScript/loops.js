3640e3cec02b335c6663ecddeb6f9378a48fabce
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-16 09:48:34 +0530

    Add files via upload

---
+++ JavaScript/loops.js
@@ -0,0 +1,51 @@
+//for loop
+// for(initialization; condition; updation/increment){
+//     code to run
+// }
+
+
+// for(let i=0; i<10; i++){
+//     console.log("hello and welcome")
+// }
+// for(let i=0; i<=10; i++){
+//     console.log("printing Hello",i, "time")
+// }
+
+//Calculate the sum of first 10 natural numbers
+// let sum = 0;
+// for(let i=1; i<=10;i++){
+//     sum += i
+// }
+// console.log("The sum of the first 10 natural numbers is: ", sum)
+
+
+
+//while loop
+// while(condition){
+//     code to run
+// }
+
+// let i=0;  //initialization
+// while(i<10){  //condition 
+//     console.log("Hello and Welcome");
+//     i++; //updation/increment
+// }
+
+//do...while loop
+// do{
+//     code to run
+// }while(condition);
+
+// let i=11;
+// do{
+//     console.log("Hello and Welcome")
+//     i++;
+// }while(i<10);
+
+// NaN...Not a Number
+// let a = "abcd";
+// let b = 2;
+// console.log(a/b)
+// console.log("abc" - "def")
+// console.log(typeof (a/b))
+// console.log(NaN==NaN)
