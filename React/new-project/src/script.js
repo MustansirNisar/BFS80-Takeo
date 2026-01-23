4774fc61786a14a418dba6c40f4f906b6cacbb8f
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-23 09:56:19 +0530

    Add files via upload

---
+++ React/new-project/src/script.js
@@ -0,0 +1,22 @@
+let employee = {
+    fName: "Sarbesh",
+    id: 2,
+    salary: 400000
+}
+// let num = 2;
+// console.log(employee.id)
+// console.log(employee.fName)
+
+// let{fName} = employee;
+// let{id} = employee;
+// let{salary} = employee;
+let{fName,id,salary} = employee;
+console.log(fName)
+console.log(id)
+console.log(id)
+console.log(salary)
+// console.log(country)
+
+let fruits = ["Apple", "Banana", "Grapes"]
+let[f1, f2, f3] = fruits;
+console.log(f1, f2, f3)
