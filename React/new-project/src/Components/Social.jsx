598f629a5732175d628e45b03ca4706f98d107a9
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-23 09:54:38 +0530

    Add files via upload

---
+++ React/new-project/src/Components/Social.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Social({name, age, country}) {
+  return (
+    <div>
+      <h1>This is the Social Media Section: </h1>
+      <h4>Name:{name}</h4>
+      <h4>Age:{age}</h4>
+      <h4>Country:{country}</h4>
+    </div>
+  )
+}
+
+export default Social
