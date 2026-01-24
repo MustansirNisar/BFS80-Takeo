6bdb6774ebc046a2c6f7c3f76fe78e45c5a3f1cc
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-24 09:57:45 +0530

    Update assignmentop.js

--- JavaScript/assignmentop.js
+++ JavaScript/assignmentop.js
@@ -1,9 +1,9 @@
-// let a = 10;
-// // a = a + 5;
-// // a = a - 5
-// // a = a / 2
-// a = a * 2
-// console.log(a)
+ let a = 10;
+ a = a + 5;
+ a = a - 5
+ a = a / 2
+ a = a * 2
+ console.log(a)
 
 // let a = 10;
 // a += 5;
@@ -13,3 +13,4 @@
 // a %= 3
 // a **= 2  //Compound Assignment Operators
 // console.log(a)
+
