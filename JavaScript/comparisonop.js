80e9a5cd6aaca7614f5ecb2732bdbfe3b484e45e
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-24 09:58:53 +0530

    Update comparisonop.js

--- JavaScript/comparisonop.js
+++ JavaScript/comparisonop.js
@@ -1,11 +1,11 @@
 // let a = 10;
 // let b = 5;
 // let c = "10";
-// console.log(a == c)  //true...checks only the value
-// console.log(a === c) //false...checks both the value and type
-// console.log(a != c)
-// console.log(a !== c)
-// console.log(a == c)
+ console.log(a == c)  //true...checks only the value
+ console.log(a === c) //false...checks both the value and type
+ console.log(a != c)
+ console.log(a !== c)
+ console.log(a == c)
 
 // console.log(a > b)
 // console.log(a < b)
@@ -29,3 +29,4 @@
 // let a = "1234"
 // let b = Number(a)
 // console.log(typeof b)
+
