1631ed2d9a4f7be6ca6abb7d6ce68585e5ffa05f
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-28 15:30:45 +0530

    Add files via upload

---
+++ React/new-project/src/Components/Another.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Another() {
+  return (
+    <div>
+        <h1>welcome to the child component</h1>
+      <h4>This is inside the Another Component</h4>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, ratione.</p>
+    </div>
+  )
+}
+
+export default Another
