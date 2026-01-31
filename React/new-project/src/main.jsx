9807bd61761d14546b36431e430dab7c0eeb8348
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-31 11:30:46 +0530

    Add files via upload

--- React/new-project/src/main.jsx
+++ React/new-project/src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
+// import store from './ReduxLegacy/Store.jsx'
+import storee from './ReduxToolkit/Storee.jsx'
+import {Provider} from 'react-redux'
 createRoot(document.getElementById('root')).render(
+    <Provider store = {storee}>
     <App />
+    </Provider>
+    
   
 
 )
